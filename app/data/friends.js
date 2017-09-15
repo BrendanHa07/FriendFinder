947202f0f6d7f60c7e7478d337e87ef64b884900
Brendan Ha <ha.brendan07@gmail.com>
2017-09-15 00:18:19 -0700

    Second commit

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friends = 
+[
+    {
+        "name": "Naruto",
+        "image":"http://1.bp.blogspot.com/_-h5d-Q8E5O8/ShSvcs9EopI/AAAAAAAAAhI/XL-svABgwFU/s400/watch+naruto+shippuden+episode+111.jpg",
+        "score": "[1,5,5,3,1,2,5,4,1,1]"
+    }
+]
+
+module.exports = friends;
